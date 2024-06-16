d4e2b43fc2801472617d91ff162f0a84bf627573
Riley Kopp <koppril7@gmail.com>
2024-06-16 13:47:01 -0400

    Add 4 button interface

--- code/light_controller_code/light_controller_code.ino
+++ code/light_controller_code/light_controller_code.ino
@@ -2,8 +2,11 @@ const int GREEN_PIN = 9;
 const int RED_PIN = 10;
 const int BLUE_PIN = 11;
 
-const int RED_BUTTON = 2;
-const int WHITE_BUTTON = 3;
+const int GREEN_BUTTON = 5;
+const int BLUE_BUTTON = 4; 
+const int RED_BUTTON = 3;
+const int WHITE_BUTTON = 6;
+
 
 void setup() {
   // put your setup code here, to run once:
@@ -12,6 +15,8 @@ void setup() {
   pinMode(BLUE_PIN, OUTPUT);
 
   pinMode(RED_BUTTON, INPUT);
+  pinMode(GREEN_BUTTON, INPUT);
+  pinMode(BLUE_BUTTON, INPUT);
   pinMode(WHITE_BUTTON, INPUT);
 }
 
@@ -20,20 +25,26 @@ void loop() {
 
   int whiteState = digitalRead(WHITE_BUTTON);
   int redState = digitalRead(RED_BUTTON);
+  int blueState = digitalRead(BLUE_BUTTON);
+  int greenState= digitalRead(GREEN_BUTTON);
 
-  if (whiteState) {
-    digitalWrite(GREEN_PIN, HIGH);
-    digitalWrite(RED_PIN, HIGH);
-    digitalWrite(BLUE_PIN, HIGH);
-  } 
-  else if (redState) {
-    digitalWrite(GREEN_PIN, LOW);
-    digitalWrite(RED_PIN, HIGH);
-    digitalWrite(BLUE_PIN, LOW);
-  } 
-  else {
-    digitalWrite(GREEN_PIN, LOW);
-    digitalWrite(RED_PIN, LOW);
-    digitalWrite(BLUE_PIN, LOW);
+  if(whiteState)
+  {
+
+      digitalWrite(RED_PIN, !redState);
+
+      digitalWrite(GREEN_PIN, !greenState);
+
+      digitalWrite(BLUE_PIN, !blueState);
+ 
+  }
+  else
+  {
+    
+      digitalWrite(RED_PIN, LOW);
+
+      digitalWrite(GREEN_PIN, LOW);
+
+      digitalWrite(BLUE_PIN, LOW);
   }
 }
